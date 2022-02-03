19705b1619c8659c3ae7efc3b5dd2434173639a2
Nurik97 <nurikbek999@gmail.com>
2022-02-03 14:34:48 +0600

    optomize to adaptive screen

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_calculator/constants/bmi_constants.dart';
-import 'package:my_calculator/widgets/left_bar.dart';
-import 'package:my_calculator/widgets/right_bar.dart';
+import 'package:my_calculator/ui/widgets/left_bar.dart';
+import 'package:my_calculator/ui/widgets/right_bar.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -18,56 +18,57 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
-
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: bgColor,
-        appBar: AppBar(
-          title: const Text(
-            'BMI Calculator',
-            style: TextStyle(color: textColor),
-          ),
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: const Text(
+          'BMI Calculator',
+          style: TextStyle(color: textColor),
         ),
-        body: SingleChildScrollView(
-          child: Column(children: [
-            SizedBox(height: height * .05),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              SizedBox(
-                width: width * .26,
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SafeArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            flex: 4,
+            child: Row(children: [
+              Expanded(
                 child: TextField(
+                  textAlign: TextAlign.center,
                   controller: _heightController,
                   maxLength: 5,
                   keyboardType: TextInputType.number,
                   style: f30W5.apply(color: textColor),
                   decoration: InputDecoration(
-                    border: InputBorder.none,
+                    isCollapsed: true,
+                    border: const OutlineInputBorder(),
                     hintText: 'Height',
                     hintStyle: f30W5.apply(color: textColor.withOpacity(0.8)),
                   ),
                 ),
               ),
-              SizedBox(
-                width: width * .26,
+              Expanded(
                 child: TextField(
+                  textAlign: TextAlign.center,
                   controller: _weightController,
                   maxLength: 5,
                   keyboardType: TextInputType.number,
                   style: f30W5.apply(color: textColor),
                   decoration: InputDecoration(
-                    border: InputBorder.none,
+                    isCollapsed: true,
+                    border: const OutlineInputBorder(),
                     hintText: 'Weight',
                     hintStyle: f30W5.apply(color: textColor.withOpacity(0.8)),
                   ),
                 ),
               ),
             ]),
-            SizedBox(height: height * .05),
-            GestureDetector(
+          ),
+          Expanded(
+            flex: 2,
+            child: GestureDetector(
               onTap: () {
                 double _h = double.parse(_heightController.text);
                 double _w = double.parse(_weightController.text);
@@ -84,24 +85,28 @@ class _MainScreenState extends State<MainScreen> {
               },
               child: const Text('Calculate', style: f26W5),
             ),
-            SizedBox(height: height * .1),
-            Text(_bmiResult.toStringAsFixed(2), style: f36W5),
-            SizedBox(height: height * .08),
-            Visibility(
+          ),
+          Expanded(
+              flex: 2,
+              child: Text(_bmiResult.toStringAsFixed(2), style: f36W5)),
+          Expanded(
+            flex: 2,
+            child: Visibility(
               visible: _textResult.isNotEmpty,
               child: Text(_textResult, style: f36W5),
             ),
-            SizedBox(height: height * .07),
-            Stack(children: const [
+          ),
+          Expanded(
+            flex: 8,
+            child: Stack(children: const [
               RightBars(),
-              SizedBox(height: 20.0),
               Padding(
                 padding: EdgeInsets.only(top: 50.0),
                 child: LeftBars(),
               ),
             ]),
-          ]),
-        ),
+          ),
+        ]),
       ),
     );
   }
